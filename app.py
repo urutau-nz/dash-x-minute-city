89d9179a66926f7fc29945c53edc43a886fde416
Tom Logan <tomlogan@umich.edu>
2020-05-16 12:29:19 +1200

    finishing first page

--- app.py
+++ app.py
@@ -5,16 +5,15 @@ import dash_html_components as html
 from dash.dependencies import Input, Output
 from pages import (
     overview,
-    pricePerformance,
-    portfolioManagement,
-    feesMins,
-    distributions,
-    newsReviews,
+    resilience,
+    equity,
+    recover,
+    transform,
 )
 
 app = dash.Dash(
     __name__, meta_tags=[{"name": "viewport", "content": "width=device-width"}],
-    url_base_pathname='/resilience-equity/',
+    # url_base_pathname='/resilience-equity/',
 )
 server = app.server
 
@@ -28,29 +27,26 @@ app.layout = html.Div(
 # Update page
 @app.callback(Output("page-content", "children"), [Input("url", "pathname")])
 def display_page(pathname):
-    if pathname == "/dash-financial-report/price-performance":
-        return pricePerformance.create_layout(app)
-    elif pathname == "/dash-financial-report/portfolio-management":
-        return portfolioManagement.create_layout(app)
-    elif pathname == "/dash-financial-report/fees":
-        return feesMins.create_layout(app)
-    elif pathname == "/dash-financial-report/distributions":
-        return distributions.create_layout(app)
-    elif pathname == "/dash-financial-report/news-and-reviews":
-        return newsReviews.create_layout(app)
-    elif pathname == "/dash-financial-report/full-view":
+    if pathname == "/resilience":
+        return resilience.create_layout(app)
+    elif pathname == "/equity":
+        return equity.create_layout(app)
+    elif pathname == "/recover":
+        return recover.create_layout(app)
+    elif pathname == "/transform":
+        return transform.create_layout(app)
+    elif pathname == "/all":
         return (
             overview.create_layout(app),
-            pricePerformance.create_layout(app),
-            portfolioManagement.create_layout(app),
-            feesMins.create_layout(app),
-            distributions.create_layout(app),
-            newsReviews.create_layout(app),
+            resilience.create_layout(app),
+            equity.create_layout(app),
+            recover.create_layout(app),
+            transform.create_layout(app),
         )
     else:
         return overview.create_layout(app)
 
 
 if __name__ == "__main__":
-    # app.run_server(debug=True)
-    app.run_server(port=9006)
+    app.run_server(debug=True)
+    # app.run_server(port=9006)
