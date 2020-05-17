6952bd531074a72c7ee84150000bbebfc959aade
Tom Logan <tomlogan@umich.edu>
2020-05-17 17:53:29 +1200

    adding the equity page

--- app.py
+++ app.py
@@ -22,7 +22,7 @@ from pages import (
 
 app = dash.Dash(
     __name__, meta_tags=[{"name": "viewport", "content": "width=device-width"}],
-    # url_base_pathname='/resilience-equity/',
+    url_base_pathname='/resilience-equity/',
 )
 server = app.server
 
@@ -237,5 +237,5 @@ def update_ecdf(
 
 
 if __name__ == "__main__":
-    app.run_server(debug=True)
-    # app.run_server(port=9006)
+    # app.run_server(debug=True)
+    app.run_server(port=9006)
